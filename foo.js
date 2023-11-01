426c418c466f774181f4b85541f2b8c7df721035
BK-Nywall <jonas.nywall@botkyrka.se>
2023-11-01 15:47:22 +0100

    template selector

---
+++ foo.js
@@ -0,0 +1,28 @@
+{
+    "id": 3108,
+    "address": "STORSKIFTESVÄGEN 112",
+    "postalCode": "14560",
+    "city": "NORSBORG",
+    "format": "GPS",
+    "longitude": 17.863189,
+    "latitude": 59.236187,
+    "href": "/installations/3108",
+    "meterHistory": [
+        {
+            "meter": {
+                "id": 2433,
+                "href": "/meters/2433"
+            },
+            "fromDateUTC": "0001-01-01T00:00:00Z",
+            "toDateUTC": "2021-10-21T08:28:33Z"
+        },
+        {
+            "meter": {
+                "id": 2433,
+                "href": "/meters/2433"
+            },
+            "fromDateUTC": "2021-10-21T08:28:33Z",
+            "toDateUTC": "9999-12-31T23:59:59.9999999Z"
+        }
+    ]
+}
