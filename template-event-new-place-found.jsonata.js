d068f8b362ec756b75fb0801df4d8310f7442582
BK-Nywall <jonas.nywall@botkyrka.se>
2023-11-01 15:44:06 +0100

    Files added

---
+++ template-event-new-place-found.jsonata.js
@@ -0,0 +1,13 @@
+{
+    "eventtype": "new place found",
+    "eventtypeversion": 1,
+    "objectid": object.urn,
+    "objectname" : object.urn,
+    "objecttype": "place",
+    "objectdomain": "geo",
+    "objectversionread": 0,
+    "objectversionwritten": 1,
+    "publisherid": publisherid,
+    "correlationid": correlationid,
+    "payload" : object
+}
